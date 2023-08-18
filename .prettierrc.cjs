0f9663da0ce8d1afd2f928ae012670f5872d61aa
Bastean <bastean@pm.me>
2023-08-17 21:42:07 -0300

    feat(*): add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+	/*
+	 * Rules
+	 */
+	printWidth: 100,
+	quoteProps: 'consistent',
+	singleAttributePerLine: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	useTabs: true
+};
